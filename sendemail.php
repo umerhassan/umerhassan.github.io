e69d35388a2a03cebda26874ba2a07e33ac55b28
umerhassan <33638819+umerhassan@users.noreply.github.com>
2018-07-08 17:44:25 -0600

    content changes

--- sendemail.php
+++ sendemail.php
@@ -6,7 +6,7 @@
     header( 'Content-type: application/json' );
 
 
-    $to             = 'themehippo@gmail.com'; //put your email here
+    $to             = 'omerhassan0001@gmail.com'; //put your email here
     $email_template = 'simple.html';  // will find it on email-templates/ directory
 
     $subject    = "SUBJECT";
